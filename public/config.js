da52f637fab1e881c49305b62a198779fdcd7f1d
KIMSEONHO <sinho0689@gmail.com>
2017-07-30 00:11:37 +0900

    - init commit, login 페이지 추가
    Signed-off-by: KIMSEONHO <sinho0689@gmail.com>

---
+++ public/config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by insu on 2017-07-26.
+ */
+var config = {
+    apiKey: "AIzaSyBgcSHxbo93P-N9Sh-ZIqgzvwicHDk_Skc",
+    authDomain: "fir-ui-demo-84a6c.firebaseapp.com",
+    databaseURL: "https://fir-ui-demo-84a6c.firebaseio.com",
+    storageBucket: "fir-ui-demo-84a6c.appspot.com",
+};
+firebase.initializeApp(config);
